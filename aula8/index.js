ffcb7572c42372e8a7f34a36b756eee6d632fc02
Anderson Leite <anderson.leite.bsi@gmail.com>
2020-06-30 10:17:04 -0300

    Exercícios - Const e Let

---
+++ aula8/index.js
@@ -0,0 +1,15 @@
+const nome = 'Anderson';
+const sobrenome = 'Leite';
+const idade = 23;
+const peso = 73;
+const alturaEmM = 1.75;
+let indiceMassaCorporal; // peso / (altura * altura)
+let anoNascimento;
+
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+anoNascimento = 2019 - idade;
+
+// template strings
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
