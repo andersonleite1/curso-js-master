f474176ac175d0d5472c561a3f14892530820182
Anderson Leite <anderson.leite.bsi@gmail.com>
2020-07-07 08:27:42 -0300

    Mais diferenças entre var e let/const

---
+++ aula31/index.js
@@ -0,0 +1,37 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco { ... bloco }
+// Var só tem escopo de função
+
+// let nome = 'Anderson'; // criando
+// var nome2 = 'Anderson'; // criando
+
+
+// if (verdadeira) {
+//   let nome = 'Joao'; // criando
+//   var nome2 = 'Pedro'; // redeclarando
+
+//   if (verdadeira) {
+//     var nome2 = 'Ronaldo'; // redeclarando
+//     let nome = 'Outra coisa';
+//   }
+// }
+
+// console.log(nome, nome2);  
+
+
+// function falaOi () {
+
+//   if (verdadeira) {
+//     let nome = 'Anderson';
+//     var sobrenome = 'Leite';
+//   }
+
+//   console.log(sobrenome);
+
+// }
+
+// falaOi();
+
+let sobrenome = 'Leite';
+console.log(sobrenome);
