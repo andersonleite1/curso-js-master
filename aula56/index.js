fa281f39197e2ca541931d4e254371c4b80892cc
Anderson Leite <anderson.leite.bsi@gmail.com>
2020-07-13 12:06:19 -0300

    Funções fábrica (Factory Functions)

---
+++ aula56/index.js
@@ -0,0 +1,41 @@
+// Factory function (Função fábrica)
+function criaPessoa(nome, sobrenome, altura, peso) {
+  return {
+    nome,
+    sobrenome,
+
+    // Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(' ');
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(' ');
+    },
+
+    fala(assunto = 'falando sobre JS') {
+      return `${this.nome} está ${assunto}.`;
+    },
+
+    altura: altura,
+    peso: peso,
+
+    // Getter
+    get imc() {
+      const indice = this.peso / (this.altura ** 2);
+      return indice.toFixed(2);
+    }
+  };
+}
+
+const p1 = criaPessoa('Carla', 'Lima', 1.63, 60);
+const p2 = criaPessoa('João', 'Silvs', 1.90, 83);
+const p3 = criaPessoa('Junior', 'Coelho', 1.6, 90);
+
+console.log(p1.imc);
+console.log(p2.imc);
+console.log(p3.imc);
+
