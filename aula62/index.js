d1ddd7f20f3355626f8bc35060560c0db2d8028c
Anderson Leite <anderson.leite.bsi@gmail.com>
2020-07-16 20:10:26 -0300

    Revisão do básico em Arrays

---
+++ aula62/index.js
@@ -0,0 +1,11 @@
+// Valor por referência
+//                    0         1        2        3          4
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+// const nome = 'joao silva';
+// const nomes = nome.split(' ');
+const nomes = [ 'Joao', 'Luis', 'Carlos' ];
+const nome = nomes.join(' ');
+console.log(nome);
